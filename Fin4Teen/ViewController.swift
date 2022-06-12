1dec5e392e280ac2e0a4785a18caf95b642bab8d
Leonardo <leojportes@gmail.com>
2022-06-12 13:57:42 -0300

    Initial Commit

---
+++ Fin4Teen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fin4Teen
+//
+//  Created by Leonardo Portes on 12/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
