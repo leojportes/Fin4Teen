0262053f676245c5bc3854b9891c1ba2d2324a7b
Leonardo <leojportes@gmail.com>
2022-06-19 20:18:26 -0300

    changes in recommendation detail view

--- Fin4Teen/Resource/Infrastructure/Extensions/UIImageView+Extension.swift
+++ Fin4Teen/Resource/Infrastructure/Extensions/UIImageView+Extension.swift
@@ -7,26 +7,36 @@
 
 import UIKit
 
-var imageCache = NSCache<AnyObject, AnyObject>()
-
 extension UIImageView {
+    
     func load(urlString: String) {
-        if let image = imageCache.object(forKey: urlString as NSString) as? UIImage {
+        var task: URLSessionDataTask!
+        let imageCache = NSCache<AnyObject, AnyObject>()
+        
+        guard let url: URL = URL(string: urlString) else { return }
+        image = nil
+        
+        if let task = task {
+            task.cancel()
+        }
+        
+        if let image = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
             self.image = image
             return
         }
         
-        guard let url = URL(string: urlString) else { return }
-        
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        imageCache.setObject(image, forKey: urlString as NSString)
-                        self?.image = image
-                    }
-                }
+        task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let newImage = UIImage(data: data) else {
+                print("Couldn't load image from url: \(urlString)")
+                return
+            }
+            
+            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+            
+            DispatchQueue.main.async {
+                self.image = newImage
+            }
         }
-        }
+        task.resume()
     }
 }
--- Fin4Teen/Source/View/Common/RecommendationsDetailView.swift
+++ Fin4Teen/Source/View/Common/RecommendationsDetailView.swift
@@ -10,11 +10,13 @@ import UIKit
 class RecommendationsDetailView: UIView, ViewCodeContract {
     
     // MARK: - Properties
+    let alphaValue: CGFloat = 0.95
     
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.setColor(color: .grayDarkHigh)
+        self.alpha = alphaValue
     }
     
     required init?(coder: NSCoder) {
@@ -25,18 +27,48 @@ class RecommendationsDetailView: UIView, ViewCodeContract {
         super.layoutSubviews()
         setupView()
     }
+
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView(frame: .zero)
+        scroll.showsVerticalScrollIndicator = false
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var baseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.setColor(color: .grayDarkHigh)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     private lazy var posterView: UIImageView = {
         let image = UIImageView()
         image.backgroundColor = UIColor.setColor(color: .grayDarkHigh)
+        image.roundCorners(cornerRadius: 7)
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
-    
+
+    private lazy var gripView: UIView = {
+        let grip = UIView()
+        grip.backgroundColor = .lightGray
+        grip.roundCorners(cornerRadius: 2)
+        grip.translatesAutoresizingMaskIntoConstraints = false
+        return grip
+    }()
+
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.tintColor = .black
-        label.text = "titulo"
+        label.textColor = .white
+        label.font = UIFont.teenBoldFont.withSize(17)
+        label.numberOfLines = .zero
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -49,34 +81,86 @@ class RecommendationsDetailView: UIView, ViewCodeContract {
     
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.teenMediumFont.withSize(15)
+        label.textColor = UIColor.setColor(color: .grayLight)
+        label.backgroundColor = .clear
+        label.numberOfLines = .zero
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     // MARK: - Setup viewcode
     func setupHierarchy() {
-        self.addSubview(posterView)
-        self.addSubview(titleLabel)
+        self.addSubview(gripView)
+        self.addSubview(baseView)
+        baseView.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+        
+        containerView.addSubview(posterView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(descriptionLabel)
+        
+        containerView.addSubview(releaseLabel)
     }
     
     func setupConstraints() {
-        posterView
-            .topAnchor(in: self, padding: 100)
-            .heightAnchor(250)
-            .widthAnchor(180)
+        
+        gripView
+            .topAnchor(in: self, padding: 10)
             .centerX(in: self)
-
-        titleLabel
-            .topAnchor(in: posterView, attribute: .bottom, padding: 30)
+            .heightAnchor(5)
+            .widthAnchor(35)
+        
+        baseView
+            .topAnchor(in: self, padding: 30)
             .leftAnchor(in: self)
             .rightAnchor(in: self)
-            .heightAnchor(30)
+            .bottomAnchor(in: self, layoutOption: .useMargins)
+        
+        scrollView
+            .topAnchor(in: baseView)
+            .leftAnchor(in: baseView)
+            .rightAnchor(in: baseView)
+            .bottomAnchor(in: baseView, layoutOption: .useSafeArea)
+        
+        containerView
+            .pin(toEdgesOf: scrollView)
+        containerView
+            .widthAnchor(in: scrollView, 1)
+            .heightAnchor(in: scrollView, 1, withLayoutPriorityValue: 250)
+        
+        posterView
+            .topAnchor(in: containerView, padding: 20)
+            .heightAnchor(280)
+            .widthAnchor(190)
+            .leftAnchor(in: containerView, padding: 15)
+        
+        releaseLabel
+            .topAnchor(in: containerView, padding: 20)
+
+        titleLabel
+            .topAnchor(in: containerView, padding: 25)
+            .leftAnchor(in: posterView, attribute: .right, padding: 15)
+            .rightAnchor(in: containerView, padding: 15)
+        
+        descriptionLabel
+            .topAnchor(in: posterView, attribute: .bottom, padding: 25)
+            .leftAnchor(in: containerView, padding: 15)
+            .rightAnchor(in: containerView, padding: 15)
     }
     
     // MARK: Bind model - view
     func bind(indexPath: IndexPath, value: [Movie]) {
         self.posterView.load(urlString: value[indexPath.row].url_poster)
         self.titleLabel.text = value[indexPath.row].title
+        self.descriptionLabel.text = value[indexPath.row].description
     }
 
+}
+
+extension RecommendationsDetailView: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset.y)
+    }
 }
